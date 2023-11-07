315d64cf2e78274baa2dbcf21d7eb93d96ca36e0
nada <nadasamir9334@gmail.com>
2023-11-07 08:42:54 -0500

    remove unneeded component

--- components/span/change.tsx
+++
@@ -1,7 +0,0 @@
-export function Change({ value }: { value: number }) {
-  return (
-    <span className={value === 0 ? 'text-gray-300' : value > 0 ? 'text-green-500' : 'text-red-500'}>
-      {value.toFixed(2)}%
-    </span>
-  )
-}
