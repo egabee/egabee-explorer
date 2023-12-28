26480510fbf01d3f22fcbd2d6ac1847f7a56b32f
MuhammedMohsenn <muhammedmohsen8695@gmail.com>
2023-12-28 17:17:20 +0200

    added arrow btn/ deleted navbar

--- components/siteHeader.tsx
+++ components/siteHeader.tsx
@@ -1,42 +1,74 @@
-import React from 'react'
-import { PiBugFill } from 'react-icons/pi'
-import Link from 'next/link'
-import { Button, buttonVariants } from '@/components/ui/button'
-
+import React from "react";
+import { PiBugFill } from "react-icons/pi";
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import clsx from "clsx";
+import { Search } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 function SiteHeader() {
+  const pathname = usePathname();
+  const isExplorerPage = pathname === "/explorer";
   return (
-    <div className="flex flex-row w-full bg-[#19191A] text-white py-2 pl-5 sm:px-5 sm:py-2  border-b border-b-[#232326] z-70 ">
+    <div className="flex flex-row items-center w-full bg-[#19191A] text-white py-2 pl-5 sm:px-5 sm:py-2  border-b border-b-[#232326] z-70 ">
       {/* ----------------------------------LOGO-------------------------------- */}
       <div className="mt-2">
-        <div className="inline-flex">
-          <PiBugFill
-            className={`bg-light-yellow text-black text-3xl rounded cursor-pointer block float-left mr-2 duration-500`}
-          />
-          <h1 className={`text-white origin-left font-medium text-2xl duration-300`}>Egabee</h1>
+        <div>
+          <Link href={"/"} className="inline-flex">
+            <PiBugFill
+              className={`bg-light-yellow text-black text-3xl rounded cursor-pointer block float-left mr-2 duration-500`}
+            />
+            <h1
+              className={`text-white origin-left font-medium text-2xl duration-300`}
+            >
+              Egabee
+            </h1>
+          </Link>
         </div>
       </div>
+      {/* ----------------------------------SEARCH-BAR-------------------------------- */}
+      {isExplorerPage && (
+        <div className="flex justify-center w-full">
+          <div
+            className={clsx(`relative flex justify-center items-center py-1 px-2 md:w-1/2 w-4/5 
+          border border-light-white rounded-sm gap-x-2 `)}
+          >
+            <div className="flex items-center">
+              <span className="text-[#9A999F] cursor-pointer hover:text-gray-100">
+                <Search size={20} />
+              </span>
+            </div>
+
+            <input
+              className={clsx(
+                `p-1 text-white w-full bg-[#19191A] border-none hover:ring-0 focus:outline-none focus:ring-0`
+              )}
+              type="text"
+              placeholder="Search for any transaction or address"
+              value={""}
+            />
+          </div>
+        </div>
+      )}
+      {/* ----------------------------------GO tO Explorer BTN-------------------------------- */}
       <div className="mr-5 flex flex-1 items-center justify-end space-x-4">
         <nav className="flex items-center space-x-1">
-          <Link href={'/explorer'}>
-            <div className="flex items-center  ">
-              <Button
-                variant={'outline'}
-                className="flex gap-x-2 bg-transparent text-supernova rounded hover:bg-dark-yellow hover:bg-opacity-25 hover:text-supernova border border-light-yellow"
-              >
-                <p>Go to Explorer</p>
-                {/* <LayoutDashboardIcon size={16} /> */}
-              </Button>
-            </div>
-          </Link>
-
-          {/* <ThemeToggle /> */}
+          {!isExplorerPage && (
+            <Link href={"/explorer"}>
+              <div className="flex items-center">
+                <Button
+                  variant={"outline"}
+                  className="flex gap-x-2 bg-transparent text-supernova rounded hover:bg-dark-yellow hover:bg-opacity-25 hover:text-supernova border border-light-yellow"
+                >
+                  <p>Go to Explorer</p>
+                </Button>
+              </div>
+            </Link>
+          )}
         </nav>
       </div>
     </div>
-
-
-  )
+  );
 }
 
-export default SiteHeader
+export default SiteHeader;
