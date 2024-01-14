7aa3137b7c9ad181d89a04eafd9a7c68a8741646
zeyad belal <zeyadbelal00@gmail.com>
2024-01-14 17:04:25 +0200

    nft display done

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
       <SiteHeader hideSearch={true} />
       <div className="flex justify-center min-w-[60%]  text-white py-2 pl-5 sm:px-5 sm:py-2  z-70  ">
         <div className="mt-40 flex justify-center flex-col min-w-[60%] ">
-          <p className="font-bold text-5xl text-center mb-7 text-light-yellow ">
+          <p className="font-bold text-6xl text-center mb-8 text-light-yellow ">
             Egabee
           </p>
           <SearchBar mainSearch={true} />
