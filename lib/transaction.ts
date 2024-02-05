91de8832f7755464c5b7cef50e08bf88ba03110a
nada <nadasamir9334@gmail.com>
2024-02-05 16:40:48 +0200

    change transaction details page to the same style in egabaa web

--- lib/transaction.ts
+++ lib/transaction.ts
@@ -1,63 +1,58 @@
-
-
 export interface Transaction {
-  txHash: string
-  status: 'success' | 'failed'
-  from: string
-  to: string
-  time: string
-  function: string
+  txHash: string;
+  status: "success" | "failed";
+  from: string;
+  to: string;
+  time: string;
+  function: string;
 }
 
 export interface Tx {
   contracts: {
-    id: string
-    contractName: string
-    contractAddress: string
-  }[]
-  address: string
-  blockNumber: number
-  chainId: string
+    id: string;
+    contractName: string;
+    contractAddress: string;
+  }[];
+  address: string;
+  blockNumber: number;
+  chainId: string;
   events: {
     attributes: {
-      key: string
-      value: string
-    }[]
-    type: string
-  }[]
-  gasUsed: number
-  gasWanted: number
-  id: string
-  log: null
+      key: string;
+      value: string;
+    }[];
+    type: string;
+  }[];
+  gasUsed: number;
+  gasWanted: number;
+  id: string;
+  log: null;
   messages: {
-    type: string
-    value: string
-  }[]
-  success: boolean
-  timestamp: number
+    type: string;
+    value: string;
+  }[];
+  success: boolean;
+  timestamp: number;
+  txType: string;
 }
-
 
 export type Attribute = {
-  key: string
-  value: string
-}
+  key: string;
+  value: string;
+};
 
 export type Event = {
-  type: string
-  attributes: Attribute[]
-}
+  type: string;
+  attributes: Attribute[];
+};
 export type Message = {
-  type: string
-  value: string
-}
+  type: string;
+  value: string;
+};
 export type AggregatedMessage = {
-  type: string
-  values: string[]
-}
+  type: string;
+  values: string[];
+};
 
-
-
-
-
-export const isValidTxHash = (searchText: string) => /^[0-9a-fA-F]{64}$/.test(searchText)
+export const isValidTxHash = (searchText: string) =>
+  /^[0-9a-fA-F]{64}$/.test(searchText);
